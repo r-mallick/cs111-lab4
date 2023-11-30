6d369265677603bc963ec1261fdfd640b820d106
r-mallick <rmallick0155@gmail.com>
2023-11-30 11:52:57 -0800

    asdf

--- ext2-create.c
+++ ext2-create.c
@@ -293,7 +293,9 @@ void write_block_bitmap(int fd)
 	for (int i = 0; i < BLOCK_SIZE; i++) {
 		if (i == 2) {
 			map_value[i] = 0x7f;
-		} else if (i > 2 && i < 128) {
+		} else if (i == 128) {
+			map_value[i] = 0xf7
+		} else if (i > 2 && i < 127) {
 			map_value[i] = 0x00;
 		} else {
 			map_value[i] = 0xff;
