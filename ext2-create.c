0990be9140b6f51eaa101f82b413f1b96375f44c
r-mallick <rmallick0155@gmail.com>
2023-11-30 11:08:29 -0800

    maybe the block bit map works

--- ext2-create.c
+++ ext2-create.c
@@ -291,10 +291,10 @@ void write_block_bitmap(int fd)
 	u8 map_value[BLOCK_SIZE];
 
 	for (int i = 0; i < BLOCK_SIZE; i++) {
-		if (i == 0) {
-			map_value[i] = 0x00;
-		} else {
+		if (i <= 3) {
 			map_value[i] = 0xff;
+		} else {
+			map_value[i] = 0x00;
 		}
 	}
 	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
