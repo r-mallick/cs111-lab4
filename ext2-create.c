2e96e6300ea5457b9720f57091bc6910bec8abe8
r-mallick <rmallick0155@gmail.com>
2023-11-30 12:05:07 -0800

    asdf

--- ext2-create.c
+++ ext2-create.c
@@ -317,6 +317,15 @@ void write_inode_bitmap(int fd)
 
 	// TODO It's all yours
 	u8 map_value[BLOCK_SIZE];
+	for (int i = 0; i < BLOCK_SIZE; i++) {
+		if (i == 1) {
+			map_value[i] = 1F;
+		} else if (i > 1 && <= 15) {
+			map_value[i] = 0x00;
+		} else {
+			map_value[i] = 0xff;
+		}
+	}
 
 	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
 	{
