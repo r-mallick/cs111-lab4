5f638f01acf2930c218dc9878adbed5f6b511dce
r-mallick <rmallick0155@gmail.com>
2023-11-30 22:40:06 -0800

    asdf

--- ext2-create.c
+++ ext2-create.c
@@ -435,7 +435,7 @@ void write_inode_table(int fd) {
 	hello_sym_link_inode.i_gid = 1000;
 	hello_sym_link_inode.i_links_count = 1;
 	hello_sym_link_inode.i_blocks = 2; /* These are oddly 512 blocks */
-	hello_sym_link_inode.i_block[0] = 13;
+	hello_sym_link_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_INO, &hello_sym_link_inode);
 
 }
