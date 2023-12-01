a827cb7b63593ca2c186b877a1290dab34d94620
r-mallick <rmallick0155@gmail.com>
2023-11-30 22:28:36 -0800

    asdf

--- ext2-create.c
+++ ext2-create.c
@@ -20,7 +20,6 @@ typedef int32_t i32;
 #define NUM_BLOCKS 1024
 #define NUM_INODES 128
 
-#define ROOT_DIR_INO       2
 #define LOST_AND_FOUND_INO 11
 #define HELLO_WORLD_INO    12
 #define HELLO_INO          13
@@ -419,7 +418,26 @@ void write_inode_table(int fd) {
 	hello_world_inode.i_blocks = 2; /* These are oddly 512 blocks */
 	hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_WORLD_INO, &hello_world_inode);
-	
+
+	// inode for hello symbolic link
+	struct ext2_inode hello_sym_link_inode = {0};
+	hello_sym_link_inode.i_mode = EXT2_S_IFREG
+	                              | EXT2_S_IRUSR
+	                              | EXT2_S_IWUSR
+	                              | EXT2_S_IRGRP
+	                              | EXT2_S_IROTH;
+	hello_sym_link_inode.i_uid = 1000;
+	hello_sym_link_inode.i_size = 11;
+	hello_sym_link_inode.i_atime = current_time;
+	hello_sym_link_inode.i_ctime = current_time;
+	hello_sym_link_inode.i_mtime = current_time;
+	hello_sym_link_inode.i_dtime = 0;
+	hello_sym_link_inode.i_gid = 1000;
+	hello_sym_link_inode.i_links_count = 1;
+	hello_sym_link_inode.i_blocks = 2; /* These are oddly 512 blocks */
+	hello_sym_link_inode.i_block[0] = 18;
+	write_inode(fd, EXT2_GOOD_OLD_FIRST_INO, &hello_sym_link_inode);
+
 }
 
 void write_root_dir_block(int fd)
